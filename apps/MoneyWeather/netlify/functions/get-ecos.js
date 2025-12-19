ef29c44be51b0f584b5df9196873a7f8fe00230c
lambdaground <lambdaground@gmail.com>
2025-12-20 02:10:45 +0900

    Update get-ecos.js

--- apps/MoneyWeather/netlify/functions/get-ecos.js
+++ apps/MoneyWeather/netlify/functions/get-ecos.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    const apiKey = process.env.ECOS_API_KEY;
+    const apiKey = process.env.VITE_ECOS_API_KEY;
     
     if (!apiKey) {
       return {
